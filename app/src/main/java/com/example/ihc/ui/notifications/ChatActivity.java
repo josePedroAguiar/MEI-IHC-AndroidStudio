89f667f6fc04682b17d80dec51ecc12b95cc8319
José Pedro Aguiar <88104767+josePedroAguiar@users.noreply.github.com>
2022-11-21 22:15:34 +0000

    Correção nos ecras

---
+++ app/src/main/java/com/example/ihc/ui/notifications/ChatActivity.java
@@ -0,0 +1,22 @@
+package com.example.ihc.ui.notifications;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.ihc.R;
+import com.example.ihc.databinding.ActivityMatchBinding;
+
+public class ChatActivity extends AppCompatActivity {
+
+    //Activity binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //binding = ActivityMatchBinding.inflate(getLayoutInflater());
+
+
+    }
+}
