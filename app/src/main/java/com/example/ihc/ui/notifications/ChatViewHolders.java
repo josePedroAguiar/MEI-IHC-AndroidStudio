9c0ef89ad5835c9221414eeb5ee0a9c8de20c458
miguelferreira2 <miguel.pampilhosa@gmail.com>
2022-12-01 23:28:56 +0000

    chat criacao incial

--- app/src/main/java/com/example/ihc/ui/notifications/ChatViewHolders.java
+++ app/src/main/java/com/example/ihc/ui/notifications/ChatViewHolders.java
@@ -1,14 +1,23 @@
 package com.example.ihc.ui.notifications;
 
+import android.content.DialogInterface;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class ChatViewHolders extends ViewModel {
+public class ChatViewHolders extends RecyclerView.ViewHolder implements DialogInterface.OnClickListener {
 
 
-    public ChatViewHolders() {
-
+    public ChatViewHolders(@NonNull View itemView) {
+        super(itemView);
     }
 
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
+    }
 }
