9a03bdd4cc9f0d4b70031cb704c83408b7e12902
miguelferreira2 <miguel.pampilhosa@gmail.com>
2022-12-07 18:44:13 +0000

    continucao mensagens

--- app/src/main/java/com/example/ihc/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/ihc/ui/notifications/NotificationsFragment.java
@@ -30,9 +30,10 @@ import java.util.List;
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
-    ArrayList<String> nomes = new ArrayList<>(Arrays.asList("Miguel", "Tiago", "José", "Pedro", "Tomás"));
-    ArrayList<String> subitems = new ArrayList<>(Arrays.asList("Miguel", "Tiago", "José", "Pedro", "Tomás"));
-    public static ArrayList<User> userMatches = new ArrayList<>();
+    //ArrayList<String> nomes = new ArrayList<>(Arrays.asList("Miguel", "Tiago", "José", "Pedro", "Tomás"));
+    //ArrayList<String> subitems = new ArrayList<>(Arrays.asList("Miguel", "Tiago", "José", "Pedro", "Tomás"));
+    ArrayList<User> messaged;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
@@ -42,14 +43,14 @@ public class NotificationsFragment extends Fragment {
 
 
 
-        String[] name = {"Christopher", "Craig", "Sergio", "Mubariz", "Mike", "Michael", "Toa", "Ivana", "Alex"};
+        /*[] name = {"Christopher", "Craig", "Sergio", "Mubariz", "Mike", "Michael", "Toa", "Ivana", "Alex"};
         String[] lastMessage = {"Heye", "Supp", "Let's Catchup", "Dinner tonight?", "Gotta go",
                 "i'm in meeting", "Gotcha", "Let's Go", "any Weekend Plans?"};
         String[] lastmsgTime = {"8:45 pm", "9:00 am", "7:34 pm", "6:32 am", "5:76 am",
                 "5:00 am", "7:34 pm", "2:32 am", "7:76 am"};
         String[] phoneNo = {"7656610000", "9999043232", "7834354323", "9876543211", "5434432343",
                 "9439043232", "7534354323", "6545543211", "7654432343"};
-        String[] country = {"United States", "Russia", "India", "Israel", "Germany", "Thailand", "Canada", "France", "Switzerland"};
+        String[] country = {"United States", "Russia", "India", "Israel", "Germany", "Thailand", "Canada", "France", "Switzerland"};*/
 
 
 
@@ -60,10 +61,17 @@ public class NotificationsFragment extends Fragment {
         }*/
 
         fetchUsers();
-        Log.e("Notifications Fragment: ",Integer.toString(userArrayList.size()));
 
-        //fetchUsers();
-        ListAdapter adapter = new ListAdapter(getActivity(), userArrayList);
+        messaged = new ArrayList<>();
+        fetchMessagedUsers();
+        Log.e("Messaged Size: ", Integer.toString(messaged.size()));
+
+
+        for(int i = 0; i < messaged.size(); i++) {
+            Log.d("TESTE NOME",messaged.get(i).getName());
+        }
+
+        ListAdapter adapter = new ListAdapter(getActivity(), messaged);  //userArrayList);
         //ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(),R.layout.activity_listview,R.id.text_view,nomes);
         ListView listView = root.findViewById(R.id.mobile_list);
         listView.setAdapter(adapter);
@@ -72,9 +80,9 @@ public class NotificationsFragment extends Fragment {
             Intent i = new Intent(getActivity(), ChatActivity.class);
 
             Bundle extras = new Bundle();
-            extras.putString("name", userArrayList.get(position).getName());
+            extras.putString("name", messaged.get(position).getName());
             //extras.putInt("imageid", userArrayList.get(position).getPhotoUri());
-            extras.putString("toID", userArrayList.get(position).getUuid());
+            extras.putString("toID", messaged.get(position).getUuid());
 
 
             i.putExtras(extras);
@@ -92,25 +100,6 @@ public class NotificationsFragment extends Fragment {
         binding = null;
     }
 
-    /*private void fetchUsers() {
-        FirebaseFirestore.getInstance().collection("/users")
-                .addSnapshotListener((value, error) -> {
-                    if (error != null) {
-                        Log.e("Teste", error.getMessage(), error);
-                        return;
-                    }
-                    assert value != null;
-                    List<DocumentSnapshot> docs = value.getDocuments();
-                    for (DocumentSnapshot doc : docs) {
-                        User user = doc.toObject(User.class);
-                        assert user != null;
-                        Log.d("Teste", user.getName());
-                        userMatches.add(user);
-                        //Log.d("Teste2", "Cheguei");
-                    }
-                });
-    }*/
-
     private void fetchUsers() {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser!=null) {
@@ -122,11 +111,13 @@ public class NotificationsFragment extends Fragment {
                         }
 
                         List<DocumentSnapshot> docs = value.getDocuments();
+                        Log.d("TAMANHO DOCS USER", Integer.toString(docs.size()));
+
 
                         for (DocumentSnapshot doc : docs) {
                             User user = doc.toObject(User.class);
                             assert user != null;
-                            Log.d("Teste", user.getName());
+                            //Log.d("Teste", user.getName());
                             int flag = 0;
                             if(user.getUuid()!=null && user.getUuid().compareTo(currentUser.getUid())!=0) {
                                 for (int i = 0; i < userArrayList.size(); i++) {
@@ -137,5 +128,43 @@ public class NotificationsFragment extends Fragment {
                             //Log.e("DEBUG-1",userArrayList.size()+"");
                         }
                     });}
+    }
+
+    private void fetchMessagedUsers() {
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String fromID = currentUser.getUid();
+
+        if (currentUser!=null) {
+            for(int i = 0; i < userArrayList.size(); i++) {
+                User thisUser = userArrayList.get(i);
+                String toID = thisUser.getUuid();
+                if(fromID.compareTo(toID) != 0 ) Log.d("Current fromID", fromID);
+                FirebaseFirestore.getInstance().collection("conversations")
+                        .document(fromID)
+                        .collection(toID)
+                        .addSnapshotListener((value, error) -> {
+                            if (error != null) {
+                                Log.e("Erro fetch messaged", error.getMessage(), error);
+                                return;
+                            }
+
+                            List<DocumentSnapshot> docs = value.getDocuments();
+                            Log.d("TAMANHO DOCS", Integer.toString(docs.size()));
+
+                            for (DocumentSnapshot doc : docs) {
+                                Chat chat = doc.toObject(Chat.class);
+                                //assert chat != null;
+                                if (chat != null) {
+                                    messaged.add(thisUser);
+                                    Log.d("Teste log", thisUser.getName());
+                                }
+                                else Log.d("Chat nulo", "erroooooooooooooooooo");
+
+                            }
+                        });
+            }
+
+        }
     }
 }
